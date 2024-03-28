6e991323bf0a44dab226a2aec3c89f7d3f954e0c
Aria Paryab <aria3ppp@gmail.com>
2024-03-28 12:17:27 +0330

    add image

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module telegram-bot-demo
 go 1.22.1
 
 require (
+	github.com/anthonynsimon/bild v0.13.0 // indirect
+	github.com/disintegration/imaging v1.6.2 // indirect
 	github.com/go-telegram/bot v1.1.7 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/spf13/cobra v0.0.5 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/image v0.15.0 // indirect
 	gopkg.in/telebot.v3 v3.2.1 // indirect
 )
--- telebot.go
+++ telebot.go
@@ -1,25 +1,29 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"image/jpeg"
 	"log"
 	"time"
 
-	tele "gopkg.in/telebot.v3"
+	"github.com/disintegration/imaging"
+	"gopkg.in/telebot.v3"
 )
 
 func runTeleBot() {
-	pref := tele.Settings{
+	pref := telebot.Settings{
 		Token:  "6636321864:AAF1KWLWVTkftegnrWu0BYG_OCFK3VYzwp0",
-		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+		Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
 	}
 
-	b, err := tele.NewBot(pref)
+	b, err := telebot.NewBot(pref)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 
-	b.Handle("shaba", func(ctx tele.Context) error {
+	b.Handle("shaba", func(ctx telebot.Context) error {
 		shaba := ctx.Message().Payload
 
 		// validate shaba
@@ -27,32 +31,57 @@ func runTeleBot() {
 		return ctx.Send("your shaba is ", shaba)
 	})
 
-	// b.Handle("/hello", func(c tele.Context) error {
-	// 	fmt.Println("new /hello request")
+	b.Handle(telebot.OnPhoto, func(ctx telebot.Context) error {
+		photo := ctx.Message().Photo
+		if photo == nil {
+			return ctx.Send("This is not a photo")
+		}
 
-	// 	fname := c.Sender().FirstName
-	// 	uname := c.Sender().Username
+		jpegImage, err := jpeg.Decode(photo.FileReader)
+		if err != nil {
+			return ctx.Send("server error: jpeg.Decode: ", err.Error())
+		}
 
-	// 	return c.Send(fmt.Sprintf("Hi %s (%s)", fname, uname))
-	// })
+		inverted := imaging.Invert(jpegImage)
 
-	// b.Handle("/bye", func(c tele.Context) error {
-	// 	fmt.Println("new /bye request")
+		buf := bytes.Buffer{}
+		if err := jpeg.Encode(&buf, inverted, nil); err != nil {
+			return ctx.Send("server error: jpeg.Encode: ", err.Error())
+		}
 
-	// 	fname := c.Sender().FirstName
-	// 	uname := c.Sender().Username
+		tp := telebot.Photo{
+			File: telebot.FromReader(&buf),
+		}
 
-	// 	return c.Send(fmt.Sprintf("Bye %s (%s)", fname, uname))
-	// })
+		return ctx.Send(tp)
+	})
 
-	// b.Handle("/foo", func(c tele.Context) error {
-	// 	fmt.Println("new /foo request")
+	b.Handle("/hello", func(c telebot.Context) error {
+		fmt.Println("new /hello request")
 
-	// 	fname := c.Sender().FirstName
-	// 	uname := c.Sender().Username
+		fname := c.Sender().FirstName
+		uname := c.Sender().Username
 
-	// 	return c.Send(fmt.Sprintf("This is foo %s (%s)", fname, uname))
-	// })
+		return c.Send(fmt.Sprintf("Hi %s (%s)", fname, uname))
+	})
+
+	b.Handle("/bye", func(c telebot.Context) error {
+		fmt.Println("new /bye request")
+
+		fname := c.Sender().FirstName
+		uname := c.Sender().Username
+
+		return c.Send(fmt.Sprintf("Bye %s (%s)", fname, uname))
+	})
+
+	b.Handle("/foo", func(c telebot.Context) error {
+		fmt.Println("new /foo request")
+
+		fname := c.Sender().FirstName
+		uname := c.Sender().Username
+
+		return c.Send(fmt.Sprintf("This is foo %s (%s)", fname, uname))
+	})
 
 	b.Start()
 }
